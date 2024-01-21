c5b68a669135c0e7437b67584e8164a0b9f42f4e
Zeeshan1234 <mayub9799@gmail.com>
2024-01-21 18:17:15 +0500

    improve username form check

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
     fetchHomePosts()
   }, [status])
 
-  if (status === "authenticated" && userInfo?.username === undefined) {
+  if (status === "authenticated" && !!userInfo && userInfo?.username === undefined) {
     return (
       <UsernameForm />
     )
